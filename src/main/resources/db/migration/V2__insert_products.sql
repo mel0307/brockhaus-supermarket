da843f98eff00245b9d5c2a750d20a1f50e8be02
melika <melika.yazdanifard@exxeta.com>
2026-05-11 07:15:20 +0200

    Fix insert query

--- src/main/resources/db/migration/V2__insert_products.sql
+++ src/main/resources/db/migration/V2__insert_products.sql
@@ -1,7 +1,6 @@
 INSERT INTO supermarket.product (product_name, product_type, product_quality, product_base_price, product_expiry_date)
 VALUES
-    ('Gouda',        'CHEESE',  80, 2.00,  CURRENT_DATE + INTERVAL '60 days'),
-    ('Brie',         'CHEESE',  32, 3.00,  CURRENT_DATE + INTERVAL '55 days'),
-    ('Rotwein 2020', 'WINE',    20, 15.00, CURRENT_DATE - INTERVAL '30 days'),
-    ('Vollkornbrot', 'BREAD',   60, 1.50,  CURRENT_DATE + INTERVAL '3 days'),
-    ('Apfel',        'DEFAULT', 70, 0.50,  NULL);
+    ('Gouda',        'CHEESE',  30, 2.00,  CURRENT_DATE + INTERVAL '60 days'),
+    ('Brie',         'CHEESE',  30, 3.00,  CURRENT_DATE + INTERVAL '55 days'),
+    ('Asbach', 'WINE',    1, 15.00, CURRENT_DATE - INTERVAL '30 days'),
+    ('Vollkornbrot', 'BREAD',   5, 1.50,  CURRENT_DATE + INTERVAL '5 days'),
